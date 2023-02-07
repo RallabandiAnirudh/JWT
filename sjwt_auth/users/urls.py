bba79c6c2aae328ac779cf69f6720117a8d033b5
AnirudhR <121166948+RallabandiAnirudh@users.noreply.github.com>
2023-02-07 19:59:53 +0530

    Add files via upload

---
+++ sjwt_auth/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from .views import *
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', user_profile.as_view(), name="user_profile"),
+    path('register/', register.as_view(), name="user_registration"),
+    path('reset/', change_password, name="change_password")
+]
